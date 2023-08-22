cdcdd008a5c7aa882602fcd252610c1cf29db2fd
sgrazys <s.grazys@gmail.com>
2023-08-23 00:01:49 +0300

    add media queries

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,3 +1,4 @@
+import Description from './Description';
 import ListItem from './ListItem';
 
 function Content({ stats }) {
@@ -6,11 +7,11 @@ function Content({ stats }) {
 			<h1 className='content__heading'>
 				Get <span>insights</span> that help your business grow.
 			</h1>
-			<p className='content__description'>
-				Discover the benefits of data analytics and make better
-				decisions regarding revenue, customer experience, and overall
-				efficiency.
-			</p>
+			<Description
+				content={
+					'Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.'
+				}
+			/>
 			<ul className='list'>
 				{stats.map((s) => (
 					<ListItem
---
+++ src/components/Description.jsx
@@ -0,0 +1,5 @@
+function Description({ content }) {
+	return <p className='content__description'>{content}</p>;
+}
+
+export default Description;
